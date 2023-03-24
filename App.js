610801fccdeda07117f2e5b493e931f201338ae5
Jonathan Engelsma <jengelsma@gmail.com>
2023-03-24 15:29:55 -0400

    Completed 09a - stubbed out app

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import ActivityScreen from './screens/ActivityScreen';
+import DashboardScreen from './screens/DashboardScreen';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ActivityScreen />
+      <DashboardScreen />
     </View>
   );
 }
